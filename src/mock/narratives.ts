3a89fadd4513e1a111ce32f33231e7a6a9af9de0
李佳帆 <3151342895@qq.com>
2026-03-22 15:23:27 +0800

    add data

--- src/mock/narratives.ts
+++ src/mock/narratives.ts
@@ -4,6 +4,8 @@ import { Narrative_sifa } from './narrative2_司法'
 import { Narrative_luxun } from './narrative3_鲁迅'
 import { Narrative_dengzhongxia } from './narrative4_邓中夏'
 import { Narrative_yundaiying } from './narrative5_恽代英'
+import { Narrative_zhangchi } from './narrative6_张炽'
+import { Narrative_yuanguoping } from './narrative7_袁国平'
 
 /** 汇总多个叙事数据源，供 store / 路由按 id 区分 */
 export const mockNarratives: Narrative[] = [
@@ -11,5 +13,7 @@ export const mockNarratives: Narrative[] = [
     Narrative_sifa, 
     Narrative_dengzhongxia, 
     Narrative_yundaiying, 
-    Narrative_luxun
+    Narrative_luxun,
+    Narrative_zhangchi,
+    Narrative_yuanguoping
 ]
